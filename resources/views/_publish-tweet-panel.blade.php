2cc24ce3bb78936939da7b427560e561e6a5a6ed
Josef <juarisco@outlook.com>
2020-04-02 17:14:08 -0500

    03-Make-the-Timeline-Dynamic

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,5 +1,7 @@
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
-    <form action="">
+    <form action="/tweets" method="POST">
+        @csrf
+
         <textarea
             name="body"
             class="w-full"
@@ -10,17 +12,22 @@
 
         <footer class="flex justify-between">
             <img
-                src="https://i.pravatar.cc/40"
-                alt=""
+                src="{{ auth()->user()->avatar }}"
+                alt="Your avatar"
                 class="rounded-full mr-2"
             >
 
             <button
                 type="submit"
                 class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white"
-            >Tweet-a-roo!
+            >
+                Tweet-a-roo!
             </button>
         </footer>
 
     </form>
+
+    @error('body')
+    <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
+    @enderror
 </div>
